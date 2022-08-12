886afb97c673cb6c97a566877dde59573a1ab05a
TA2LSM <semihsenol@gmail.com>
2022-08-12 20:11:12 +0300

    commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Comment
+//console.log('Hello World');
+
+let person = {
+  // Object Literal
+  name: 'Semih',
+  age: 39,
+  //   address,
+};
+
+// person.name = 'John'; // dot notation
+// person['name'] = 'John'; // bracket notation
+
+// let selection = 'name';     // user's selection
+// person[selection] = 'John';
+
+//console.log(person);
+
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors[0]);
+
+selectedColors.push('green'); // ya da selectedColors[2] = 'green' yazılabilir.
+console.log(selectedColors);
+
+selectedColors[2] = 23;
+console.log(selectedColors);
